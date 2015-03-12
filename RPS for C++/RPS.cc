3efe2cc98c3b09339d1db59db1bad1b6cd9d5ffc
Kyle Napod <knnapod@puc.edu>
2015-03-12 14:10:26 -0700

    Added Code
    Added number values equivalent to rock, paper, and scissors.
    Created a way for user to input a number into the game
    Created a way for the computer opponent to generate a random number in between 1 and 3

--- RPS for C++/RPS.cc
+++ RPS for C++/RPS.cc
@@ -13,6 +13,18 @@ int main()
 	while(quit == -1){
 		int player;
 		int opponent;
-	}
+		
+		cout << "1.Rock" << endl;
+		cout << "2.Paper" << endl;
+		cout << "3.Scissors" << endl;
+		
+		cin >> player;
+		cout << endl;
+		
+		randomNum = (rand()%3)+1;
+		
+		
+	
 	return 0;
 }
+
